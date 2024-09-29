2c7ecf916179d67015bcea0fbc8121f28d76070a
vovinhloc <vovinhl@yahoo.com>
2024-09-29 11:43:01 +0700

    tab navigate

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 // import HomeScreen from "./screens/HomeScreen";
 // import DetailsScreen from "./screens/DetailsScreen";
@@ -15,20 +16,21 @@ const HomeScreen = ({ navigation }) => {
   );
 };
 const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
+  return <Text>This is {route?.params?.name}'s profile</Text>;
 };
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator>
+        <Tab.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: "Welcome" }}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
