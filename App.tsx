7039d7870122979d70500416f84612140b684eb6
vovinhloc <vovinhl@yahoo.com>
2024-09-30 17:37:18 +0700

    fixed loi drawer

--- App.tsx
+++ App.tsx
@@ -1,36 +1,47 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // import HomeScreen from "./screens/HomeScreen";
 // import DetailsScreen from "./screens/DetailsScreen";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-    />
+    <GestureHandlerRootView>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigation.navigate("Profile", { name: "Jane" })}
+      />
+    </GestureHandlerRootView>
   );
 };
 const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is {route?.params?.name}'s profile</Text>;
+  return (
+    <GestureHandlerRootView>
+      <Text>This is {route?.params?.name}'s profile</Text>
+    </GestureHandlerRootView>
+  );
 };
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const MyDrawer = createDrawerNavigator();
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
+      <MyDrawer.Navigator>
+        <MyDrawer.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: "Welcome" }}
         />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
+        <MyDrawer.Screen name="Profile" component={ProfileScreen} />
+      </MyDrawer.Navigator>
     </NavigationContainer>
   );
 }
