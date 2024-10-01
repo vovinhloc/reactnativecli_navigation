3eb1c00bec52eb10c645d9efa27419d87de2a3dd
vovinhloc <vovinhl@yahoo.com>
2024-10-01 09:48:31 +0700

    note drawer Naviation

--- App.tsx
+++ App.tsx
@@ -1,47 +1,29 @@
 import "react-native-gesture-handler";
-import React from "react";
+import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Button, Text } from "react-native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+// import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { Text, View } from "react-native";
 
-// import HomeScreen from "./screens/HomeScreen";
-// import DetailsScreen from "./screens/DetailsScreen";
-
-const HomeScreen = ({ navigation }) => {
+function HomeScreen() {
   return (
-    <GestureHandlerRootView>
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-      />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+      </View>
     </GestureHandlerRootView>
   );
-};
-const ProfileScreen = ({ navigation, route }) => {
-  return (
-    <GestureHandlerRootView>
-      <Text>This is {route?.params?.name}'s profile</Text>
-    </GestureHandlerRootView>
-  );
-};
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-const MyDrawer = createDrawerNavigator();
-function App(): React.JSX.Element {
+}
+
+const Drawer = createDrawerNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <MyDrawer.Navigator>
-        <MyDrawer.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Welcome" }}
-        />
-        <MyDrawer.Screen name="Profile" component={ProfileScreen} />
-      </MyDrawer.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
