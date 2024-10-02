1e02b49acbed6449875b7808dbc404bfcf1321a4
vovinhloc <vovinhl@yahoo.com>
2024-10-02 09:19:28 +0700

    Navigator style for all header

--- App.tsx
+++ App.tsx
@@ -47,7 +47,17 @@ function SettingScreen({ route, navigation }) {
 }
 function RootScreen() {
   return (
-    <MyStack.Navigator>
+    <MyStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
       <MyStack.Screen
         name="Profile"
         component={ProfileScreen}
